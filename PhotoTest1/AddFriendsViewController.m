f92a17c2c516b2a5e0b3fca803ca3ebc718e41e8
Christopher Truman <cleetruman@gmail.com>
2014-06-27 10:40:55 -0700

    Code Cleanup and Style Changes for AddFriendsViewController
    This is a good style guide for making code easy to read and understand:
    https://github.com/wordpress-mobile/WordPress-iOS/wiki/WordPress-for-iOS-Style-Guide
    
    THe extra line breaks and spacing makes things difficult to read when other devs are contributing.

--- PhotoTest1/AddFriendsViewController.m
+++ PhotoTest1/AddFriendsViewController.m
@@ -28,44 +28,23 @@
 
 @implementation AddFriendsViewController
 
-
-
-
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    
-    
-    
+
     self.currentUser = [PFUser currentUser];
-    
-   
-    
-    
+
     [self.navigationController setNavigationBarHidden:NO];
-    
-    
-    
-    
+
     // Hide the back bar button item.
-    
-    
     [self.navigationItem setHidesBackButton:YES];
-    
     [self.navigationController setNavigationBarHidden:NO];
-    
     
     //Create right bar button.
-    
     UIImage* firstButtonImage = [UIImage imageNamed:@"nextbutton3"];
-    
     NSLog(@"size of image: %@", NSStringFromCGSize(firstButtonImage.size));
     
-    
     CGRect frame = CGRectMake(0, 0, 70, 30);
-    
     
     UIButton * someButton = [[UIButton alloc] initWithFrame:frame];
     [someButton setBackgroundImage:firstButtonImage forState:UIControlStateNormal];
@@ -78,14 +57,10 @@
     
     [self.navigationItem setHidesBackButton:YES animated:YES];
     [self.navigationItem setBackBarButtonItem:nil];
-    
-    
-    
+
     //Create left bar button.
     
     CGRect frame2 = CGRectMake(0, 0, 25, 25);
-    
-    
     
     UIImage *leftButtonImage = [UIImage imageNamed:@"backarrow2"];
     
@@ -101,72 +76,35 @@
     
     [self.navigationItem setHidesBackButton:YES animated:YES];
     [self.navigationItem setBackBarButtonItem:nil];
-    
-
-    
-    
-    //[self performSelector:@selector(myTask) withObject:nil afterDelay:0.001];
-    
-    
-    
-    
-    
-    
-    
-    
 }
 
 
 
 
 -(void)viewWillAppear:(BOOL)animated {
-    
-    
-    
-    
     [super viewWillAppear:NO];
-    
-   
-    
 }
 
 
 -(void)viewDidAppear:(BOOL)animated {
-    
     [super viewDidAppear:NO];
-    
-    
-    
     //This initializes our NSMutable Arrays that will hold the first names and phone numbers of people
     //that were not found in the Parse DB aka have not signed up for the app yet.
-    
     self.potentiaFriendsNotInParseFirstNamesArray = [[NSMutableArray alloc]init];
     self.potentiaFriendsPhoneNumberArray = [[NSMutableArray alloc]init];
-    
-    
     
     self.usersToInviteToHalfsies = [[NSMutableArray alloc]init];
     
     self.addFriendButton = [[UIButton alloc]init];
     
-    
     self.cell = [[UITableViewCell alloc]init];
-    
     
     self.numberValues = [[NSString alloc]init];
     
-    
-    
-    
     //Asks for access to Address Book.
-    
     ABAddressBookRef m_addressbook =  ABAddressBookCreateWithOptions(NULL, NULL);
     
-    
-    
     ABAddressBookCopyArrayOfAllPeople(m_addressbook);
-    
-    
     
     NSLog(@"%@", m_addressbook);
     
@@ -183,87 +121,37 @@
         ABAddressBookRequestAccessWithCompletion(m_addressbook, ^(bool granted, CFErrorRef error)
                                                  
                                                  {
-                                                     
                                                      accessGranted = granted;
-                                                     
                                                      NSLog(@"Has access been granted?: %hhd", accessGranted);
-                                                     
-                                                     
                                                      NSLog(@"Has there been an error? %@", error);
-                                                     
-                                                     
                                                      dispatch_semaphore_signal(sema);
-                                                     
                                                  });
         
         dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
-        
-        
-        
-        
     }
-    
     
     if (accessGranted == 0) {
         
         NSLog(@"Access not granted.");
         
-        //Hide the HUD
-        
-        //[self.HUD hide:YES];
-        
-        
-        
         //Segue straight to the media capture VC.
-        
         [self performSegueWithIdentifier:@"addFriendsToMediaCaptureSegue" sender:self];
-        
-        
-        
-        
-        //MediaCaptureVC *mvc = [[MediaCaptureVC alloc]init];
-        
-        //[self.navigationController pushViewController:mvc animated:NO];
-        
     }
-    
-    
-      
-    
-    
-    //If access is granted.
-    
-    if (accessGranted == 1) {
-        
-        
+    else if (accessGranted == 1) {
         NSLog(@"Access granted.");
-        
-        
         // Setup and show HUD here
         
         self.HUD = [MBProgressHUD showHUDAddedTo:self.navigationController.view animated:YES];
         self.HUD.mode = MBProgressHUDAnimationFade;
         self.HUD.labelText = @"Finding Friends";
         
-        
         NSArray *allContacts = (__bridge_transfer NSArray
                                 *)ABAddressBookCopyArrayOfAllPeople(m_addressbook);
-        
-        
-        
-        
-        
         //Loop through all contacts.
         
         int z = allContacts.count;
         
-        for (int i = 0; i < z; i++)
-            
-            
-        {
-            
-            
-            
+        for (int i = 0; i < z; i++) {
             //Place current contact in ABRecordRef
             
             ABRecordRef contactPerson = (__bridge ABRecordRef)allContacts[i];
@@ -276,18 +164,11 @@
             //Create phone number from contact's phone number property.
             
             ABMultiValueRef *phoneNumber = ABRecordCopyValue(contactPerson, kABPersonPhoneProperty);
+            NSMutableArray *numbers = [[NSMutableArray alloc] init];
             
-            
-            
-            
-            
-            NSMutableArray *numbers = [[NSMutableArray alloc]init];
-            
-            NSString *number = [[NSString alloc]init];
-            
+            NSString *number = [[NSString alloc] init];
             
             //Loop through all of contact's phone numbers.
-            
             if (phoneNumber) {
                 CFIndex numberOfPhoneNumbers = ABMultiValueGetCount(phoneNumber);
                 
@@ -295,46 +176,25 @@
                     number = (__bridge_transfer NSString *)ABMultiValueCopyValueAtIndex(phoneNumber, i);
                     CFStringRef label = ABMultiValueCopyLabelAtIndex(phoneNumber, i);
                     if (label) {
-                        
-                        if(firstName.length == 0) {
-                            
-                            
-                            //Do nothing.
-                            
-                            
-                        }
-                        
                         NSLog(@"Number about to be added for %@: %@", firstName, number);
                         
                         //Add current number to array.
-                        
                         if(firstName.length > 0) {
                             
                             [numbers addObject:number];
                             
                         }
-                        
-                    } else {
-                        
                     }
                     CFRelease(label);
-                    
-                    
                 }
                 CFRelease(phoneNumber);
             }
-            
-            
-            
-            
             
             int numbersIndex;
             
             //Loop through all of the contact's numbers.
             
             for(numbersIndex = 0; numbersIndex < numbers.count; numbersIndex++) {
-                
-                
                 NSLog(@"Final loop.");
                 
                 //Set string global to current number.
@@ -352,37 +212,23 @@
                 [self.potentiaFriendsPhoneNumberArray addObject:self.numberValues];
                 
                 NSLog(@"Final phone number array: %@", self.potentiaFriendsPhoneNumberArray);
-                
             }
-            
         }
-        
-        
-        
         
         //At this point, all of the first names are in the notInParseFirstNames array.
         
         //And all of the phone numbers are in the phone number array.
         
         //Both of the arrays should have equal counts.
-        
-        
+
         NSLog(@"Count of names: %d", self.potentiaFriendsNotInParseFirstNamesArray.count);
         NSLog(@"Count of numbers: %d", self.potentiaFriendsPhoneNumberArray.count);
-        
-        
     }
-    
-    
     
     //Set table view's datasource and delegate.
     
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
-    
-    
-    
-    
     //Hide the MBProgress HUD.
     
     [self.HUD hide:YES];
@@ -390,11 +236,7 @@
     //Reload table view data.
     
     [self.tableView reloadData];
-    
-    
 }
-
-
 
 - (void)didReceiveMemoryWarning
 {
@@ -402,64 +244,24 @@
     // Dispose of any resources that can be recreated.
 }
 
-
-
-
-
-
-
--(void)myTask {
-    
-    NSLog(@"myTask has been triggered.");
-    
-   }
-
-
-
 - (void)hudWasHidden:(MBProgressHUD *)hud {
-    
     // Remove HUD from screen when the HUD hides
     [self.HUD removeFromSuperview];
     self.HUD = nil;
 }
 
-
-
-
-
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    
     return 1;
-    
 }
-
-
-
-
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    
-    
-    
     return [self.potentiaFriendsNotInParseFirstNamesArray count];
-    
-    
-    
-    
 }
-
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    
-    
-    
     return @"Friends to Invite";
-    
 }
 
-
-
-//4
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     //Set cell identifier name. Should match identifier set in IB.
@@ -469,12 +271,7 @@
     //Create cell instance.
     
     UITableViewCell  *cell = [self.tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-    
-    
-    //If first section of table view.
-    
-    
-    
+
     //Create string from first name in array.
     
     NSString *firstNameForTableView2 = [self.potentiaFriendsNotInParseFirstNamesArray objectAtIndex:indexPath.row];
@@ -482,7 +279,6 @@
     //Create string from phone number in array.
     
     NSString *userNameForTableView2 = [self.potentiaFriendsPhoneNumberArray objectAtIndex:indexPath.row];
-    
     
     //Create cell button
     
@@ -498,7 +294,6 @@
     [addFriendButton setImage:addFriendButtonImageHighlighted forState:UIControlStateSelected];
     
     [addFriendButton addTarget:self action:@selector(handleTouchUpInsideForNonUsers:) forControlEvents:UIControlEventTouchUpInside];
-    
     
     //Set cell button's tag property.
     
@@ -516,33 +311,19 @@
     
     [cell.contentView addSubview:addFriendButton];
     
-    
     return cell;
-    
 }
-
-
-
-
-
 
 - (void)handleTouchUpInsideForNonUsers:(UIButton *)sender {
     sender.selected = !sender.selected;
-    
-    
-    
+
     UIButton *cellButton = (UIButton *)sender;
     
     NSIndexPath *indexPath = [NSIndexPath indexPathForRow:cellButton.tag inSection:0];
-    
-    
-    
-    
+
     UITableViewCell *cell = [[UITableViewCell alloc]init];
     cell = [_tableView cellForRowAtIndexPath:indexPath];
-    
-    
-    
+
     if(sender.state == 5) {
         
         //This add's the username to the array.
@@ -552,66 +333,36 @@
         [_usersToInviteToHalfsies addObject:cell.detailTextLabel.text];
         
         NSLog(@"CONTENTS OF ARRAY 3: %@", _usersToInviteToHalfsies);
-        
     } else {
-        
         //This removes the username from the array.
         
         [_usersToInviteToHalfsies removeObject:cell.detailTextLabel.text];
         
         NSLog(@"CONTENTS OF ARRAY 4: %@", _usersToInviteToHalfsies);
-        
     }
-    
-    
 }
 
-
-
 -(IBAction)finishedAddingFriends {
-    
-    
-    
     NSLog(@"Pulling phone numbers properly?: %@", _usersToInviteToHalfsies);
     
-    
-    
     //This will segue to the next VC which is the Media Capture VC.
-    
-    
     if(![_usersToInviteToHalfsies count]) {
-        
-        
-        
+
         [self performSegueWithIdentifier:@"addFriendsToMediaCaptureSegue" sender:self];
-        
-        
+
     } else {
-        
         [self performSegueWithIdentifier:@"addFriendsToSMSWindowSegue" sender:self];
         
         [self.navigationController setNavigationBarHidden:YES];
-        
     }
-    
-    
 }
 
-
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:self {
-    
-    
     if ([segue.identifier isEqualToString:@"addFriendsToSMSWindowSegue"]) {
-        
-        
-        
-        
         SMSWindowViewController *smsVC = segue.destinationViewController;
         
-        
-        NSString *stringForProperty = [[NSString alloc]init];
+        NSString *stringForProperty = [[NSString alloc] init];
         stringForProperty = @"YES";
-        
         
         //The below statements are all about passing data to the next VC so we can properly launch the SMS text message invite window.
         
@@ -620,29 +371,15 @@
         smsVC.usersToInviteToHalfsies = _usersToInviteToHalfsies;
         
         //inviteText creates the custom text message that will be loaded in the next VC. It even includes the current user's username.
-        
         NSString *inviteText = [[NSString alloc]initWithFormat:@"Hey, come join this cool new app called Halfsies, and we can go halfsies on creating photos together! My username is %@ and you can download the iPhone app here in the App Store: https://itunes.apple.com/app/id869085222", _currentUser.username];
         
         //This sets the Media Capture VC's textMessageInviteText property to the custom text object we just created.
-        
-        
         smsVC.textMessageInviteText = inviteText;
-        
-        
-        
     }
-    
-    
-    
-    
 }
 
 -(void)handleBack {
-    
-    
     [self.navigationController popViewControllerAnimated:YES];
-    
-    
 }
 
 @end
