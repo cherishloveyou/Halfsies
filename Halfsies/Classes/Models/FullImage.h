59c74d9bda90b598d6942cba392f4b0313466029
Mitchell Porter <mitchellporter@gmail.com>
2014-07-07 14:17:00 -0700

    Create Models folder and group
    Created a Models folder and group. Also added a new FullImage class.

---
+++ Halfsies/Classes/Models/FullImage.h
@@ -0,0 +1,13 @@
+//
+//  FullImage.h
+//  Halfsies
+//
+//  Created by Mitchell Porter on 7/7/14.
+//  Copyright (c) 2014 Mitchell Porter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FullImage : NSObject
+
+@end
---
+++ Halfsies/Classes/Models/FullImage.m
@@ -0,0 +1,13 @@
+//
+//  FullImage.m
+//  Halfsies
+//
+//  Created by Mitchell Porter on 7/7/14.
+//  Copyright (c) 2014 Mitchell Porter. All rights reserved.
+//
+
+#import "FullImage.h"
+
+@implementation FullImage
+
+@end
