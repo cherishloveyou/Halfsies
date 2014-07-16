cf0593925dcc157017ece49b246e120ff7fb6255
Mitchell Porter <mitchellporter@gmail.com>
2014-07-15 17:55:14 -0700

    Refactored HALMediaCaptureViewController's header
    Refactored HALMediaCaptureViewController's header file and moved
    everything to it’s class extension.

--- Halfsies/Classes/View Controllers/HALMediaCaptureVC.m
+++ Halfsies/Classes/View Controllers/HALMediaCaptureVC.m
@@ -10,12 +10,46 @@
 #import "UIImage+so1.h"
 #import <MessageUI/MessageUI.h>
 #import <CoreGraphics/CoreGraphics.h>
+#import "HALSendToFriendsViewController.h"
 
 @interface HALMediaCaptureVC () <UIActionSheetDelegate, AVCaptureFileOutputRecordingDelegate, MFMessageComposeViewControllerDelegate, AVCaptureVideoDataOutputSampleBufferDelegate>
 
 @property (strong, nonatomic) NSData *imageData;
 @property (strong, nonatomic) UIImage *image;
 @property (strong, nonatomic) UIImageView *imageView;
+@property (nonatomic, strong) AVAudioPlayer *myAudioPlayer;
+@property (nonatomic,retain) AVCaptureSession *session;
+@property (nonatomic, retain) AVCaptureStillImageOutput *stillImageOutput;
+@property (nonatomic, retain) AVCaptureVideoPreviewLayer *previewLayer;
+@property (nonatomic, retain) AVCaptureDevice *inputDevice;
+@property (nonatomic, retain) NSString *hasUserTakenAPhoto;
+@property (nonatomic, retain) CALayer *subLayer;
+@property (nonatomic, retain) UIActionSheet *xButtonAfterPhotoTaken;
+@property (nonatomic, retain) NSString *xButtonActionSheetTitle;
+@property (nonatomic, retain) AVCaptureDeviceInput *deviceInput;
+@property (nonatomic, retain) AVCaptureVideoDataOutput *videoOutput;
+@property (nonatomic, retain) IBOutlet UIView *takingPhotoView;
+@property (nonatomic, retain) IBOutlet UIView *afterPhotoView;
+@property (nonatomic, retain) IBOutlet UIImageView *topHalfView;
+@property (nonatomic, retain) IBOutlet UIView *smsWindowAfterSignup;
+@property (nonatomic, retain) NSMutableArray *usersToAddToFriendsList;
+
+
+//The NSString property below will be used to pass an NSString object like "YES" from the Add Friends View Controller. That way if the user has just finished signing up and is leaving the AddFriendsViewController, and they also have selected to send a text message invite to some of their contacts, we will place a string of "YES" in this object. Then, we can setup an if statemenet that checks for a "YES" in the object, and if it finds it, then we know to launch the SMS window.
+
+@property (nonatomic, retain) NSString *justFinishedSigningUp;
+
+//These are the properties to pass the phone numbers and the text message body with user's username to this view controller.
+
+@property (nonatomic, retain) NSArray *usersToInviteToHalfsies;
+@property (nonatomic, retain) NSString *textMessageInviteText;
+@property (strong, nonatomic) IBOutlet UIButton *toggleFlashButton;
+
+- (IBAction)stillImageCapture;
+- (IBAction)toggleFlash;
+- (IBAction)xButton;
+- (IBAction)toggleCamera;
+- (IBAction)sendButton;
 
 @end
 
@@ -37,6 +71,10 @@ static inline CGSize swapWidthAndHeight(CGSize size)
     
     return size;
 }
+
+BOOL isUsingFrontFacingCamera;
+
+float finalXValueForCrop;
 
 
 @implementation HALMediaCaptureVC
--- Halfsies/Classes/View Controllers/HALSearchViewController.m
+++ Halfsies/Classes/View Controllers/HALSearchViewController.m
@@ -8,16 +8,6 @@
 
 @implementation HALSearchViewController
 
-- (id) initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self)
-    {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void) viewDidLoad
 {
     [super viewDidLoad];
